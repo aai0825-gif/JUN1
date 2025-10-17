374918ae71ab40beb1615503fa2ca04cc0b0cb58
aai0825-gif <aai0825@gmail.com>
2025-10-17 16:58:19 +0900

    Update BootReceiver.kt

--- app/src/main/java/com/example/jun1/alarm/BootReceiver.kt
+++ app/src/main/java/com/example/jun1/alarm/BootReceiver.kt
@@ -1,7 +1,9 @@
 package com.example.jun1.alarm
+
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
